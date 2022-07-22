3e30ee0ac8c5dca409155bdd83f10535b10ec491
Aemaeth <screwthisusernameprocess@gmail.com>
2022-07-22 00:55:09 -0400

    logger/loggging

--- chat/echo.js
+++ chat/echo.js
@@ -1,4 +1,8 @@
+const {Logger, llogs} = require("../lib/logging/Logger.js") 
+
+const logger = new Logger(llogs.debug, "echo", "echo.log")
+
 // MADE BY discord: MotokoKusanagi#5346
 // echos recieved chat messages back as logs
 // sent as script to be triggered by "recvmessage" event
-Chat.log("Message: '" +event.text.getString() + "'");
+logger.log(`Message: '${event.text.getString()}'`)
--- chat/item-pickup.js
+++ chat/item-pickup.js
@@ -1,4 +1,11 @@
 // MADE BY discord: MotokoKusanagi#5346
 // log a chat message for every item you pick up
+const {Logger} = require("../lib/Logger.js") 
+
+const logger = new Logger("pickup", "pickup.log")
+
+// MADE BY discord: MotokoKusanagi#5346
+// echos recieved chat messages back as logs
+// sent as script to be triggered by "recvmessage" event
 const i = event.item;
-Chat.log(`You picked up ${i.getCount()} ${i.getName().getString()}`);
+logger.log(`You picked up ${i.getCount()} ${i.getName().getString()}`);
