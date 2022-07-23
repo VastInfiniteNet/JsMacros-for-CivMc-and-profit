708e4a750753da6f13e6f74f0de5ae40389163b8
Aemaeth <screwthisusernameprocess@gmail.com>
2022-07-22 23:32:24 -0400

    this is a mess

--- farming/wheatBot/wheatBot-icenia.js
+++ farming/wheatBot/wheatBot-icenia.js
@@ -1,8 +1,49 @@
 const {Logger} = require("../../lib/Logger.js")
-const {startEnd} = require("./selectBlock.js")
-const {centerSelf} = require("../../lib/move.js")
+const {startEnd, distFromHere} = require("./selectBlock.js")
+const {centerSelf, blocksForward} = require("../../lib/move.js")
+const {lookAngle, horLookVec, flipYaw} = require("../../lib/look.js")
+const {distance } = require("../../lib/coords.js")
 
 const logger = new Logger("wheatbot Icenian wall", "wheat.log", Logger.llog.debug, Logger.llog.debug)
+const user = Player.getPlayer()
+const start = {}
+// should follow PositionCommon.createPos(X, Y, Z)
+const width = 0
+let length = 5
 
-const [end, start] = startEnd()
-centerSelf()
+// FUNCTIONS
+/**
+ * Go through line column of crops, harvest all. 
+ * @param {*} length how long the column of crops is
+ */
+function snortLine(length) {
+    //const goalPos = 
+    KeyBind.keyBind("key.use", true);
+    blocksForward(length)
+    KeyBind.keyBind("key.use", false);
+
+}
+
+function dumpProduct() {
+
+}
+
+
+// MAIN
+function Main() {
+    //centerSelf()
+    //lookAngle(40, -90)
+
+    //const [end, start] = startEnd()
+    if (!length)
+        length = distFromHere()
+
+    //snortLine(length)
+}
+
+Main()
+
+
+
+
+
