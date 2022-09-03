b4f270536e966103a4f0afaa9af39dc248177a09
Aemaeth <screwthisusernameprocess@gmail.com>
2022-09-03 14:35:21 -0400

    isBroke docs + equilvant item check

--- inventory/replacer/isBroke.js
+++ inventory/replacer/isBroke.js
@@ -6,21 +6,21 @@
 
 
 // Imports
-const { offHandSlot, hotBarSlots } = require("../../lib/inventory.js")
-const { itemToString, itemName } = require("../../lib/item.js")
+const { offHandSlot, hotBarSlots, isSimilarItemInInventory } = require("../../lib/inventory.js")
+const { itemToString, itemName, itemId } = require("../../lib/item.js")
 const { Logger } = require("../../lib/Logger.js")
 
-const logger = new Logger("isBroke", "isBroke.log")
+const logger = new Logger("isBroke", "replace.log")
 
 // ----- CONFIG ------
-const BLACK_LIST = ["Sword", "Potion"]
+const BLACK_LIST = ["sword", "potion", "bucket"]
 
 // ----- END OF CONFIG -----
 
 /**
  * Decides if held item change was from item break/runnout or not.
- * @param {ItemStackHelper} currentItem old held item
- * @param {ItemStackHelper} oldItem     new held item
+ * @param {ItemStackHelper} currentItem current ItemStackHelper held 
+ * @param {ItemStackHelper} oldItem     old ItemStackHelper held 
  * @param {Boolean} isOffHand if item change happened in off hand
  * @returns {Boolean} whether or not an item broke/ranout
  */
@@ -28,36 +28,36 @@ const BLACK_LIST = ["Sword", "Potion"]
     const inv = Player.openInventory()
 
     const oldSlot = GlobalVars.getDouble("oldSlotIndex")
-    const oldItemName = oldItem.getName().getString()
+    const oldItemName = itemName(oldItem)
     
     var currentSlot = hotBarSlots()[0] + inv.getSelectedHotbarSlotIndex()
     if (isOffHand) {
-        logger.log("Item change in offhand", Logger.llog.info)
+        logger.log("Item change in offhand", Logger.llog.debug)
         
-        currentSlot = offHandSlot()
+        currentSlot = offHandSlot()[0]
     }
     const currentItemName = currentItem.getName().getString()
     
     GlobalVars.putDouble("oldSlotIndex", currentSlot)
 
-    logger.log(`Old item: ${itemToString(oldItem)}`, Logger.llog.debug)
-    logger.log(`New item: ${itemToString(currentItem)}`, Logger.llog.debug)
+    logger.log(`Old     item: ${itemToString(oldItem)}`, Logger.llog.debug)
+    logger.log(`Current item: ${itemToString(currentItem)}`, Logger.llog.debug)
 
     // check if inventory open
     if (Hud.getOpenScreenName() !== null) {
-        logger.log("Inventory is open!", Logger.llog.info)
+        logger.log(`Inventory ${Hud.getOpenScreenName()} is open!`, Logger.llog.info)
         return false
     }
 
     // check still in same slot
     if (currentSlot !== oldSlot) {
-        logger.log(`Different slots!: old: ${oldSlot}, current: ${currentSlot}`, Logger.llog.debug)
+        logger.log(`Different slots!: old: ${oldSlot}, current: ${currentSlot}`, Logger.llog.info)
         return false    
     }
 
     // check if 1 left
     if (oldItem.getCount() !== 1) {
-        logger.log("Not empty!", Logger.llog.info)
+        logger.log(`Not empty! ${oldItem.getCount()} left!`, Logger.llog.info)
         return false
     }
 
@@ -67,21 +67,21 @@ const BLACK_LIST = ["Sword", "Potion"]
         return false
     }
 
-    // check old item was at break durability (1)
-    if (oldItem.getMaxDamage() > 0 && oldItem.getMaxDamage() - oldItem.getDamage() !== 1) {
-        logger.log("Item still has durability!", Logger.llog.info)
+    // check old item had low durability
+    if (oldItem.getMaxDamage() > 0 && oldItem.getMaxDamage() - oldItem.getDamage() >= 5) {
+        logger.log(`Item still has durability! (${oldItem.getDamage()} left)`, Logger.llog.info)
         return false
     }
 
     // TODO: check if equivalent item is in inventory
-    if (false) {
+    if (isSimilarItemInInventory(oldItem)) {
         logger.log("Item was just moved in inventory!", Logger.llog.info)
         return false
     }
 
     // check if non-blacklisted item
-    if ( BLACK_LIST.some(e => itemName(oldItem).includes(e)) ) {
-        logger.log("Unreplacable item!", Logger.llog.info)
+    if ( BLACK_LIST.some(e => itemId(oldItem).includes(e)) ) {
+        logger.log(`Unreplacable item (${itemId(oldItem)})!`, Logger.llog.info)
         return false
     }
 
