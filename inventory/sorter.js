7de0b2f5ec043a55773004a9e85dbb4d6ccf838c
Aemaeth <screwthisusernameprocess@gmail.com>
2022-09-05 23:33:38 -0400

    invenotry sorter

--- inventory/sorter.js
+++ inventory/sorter.js
@@ -2,60 +2,84 @@
  * A simple inventory sorter.
  * Author: MotokoKusanagi#5346
  * TODO: 
- * - a better inplace sorting algorithm (quicksort)
- * - an even better algo that isn't inplace (heapsort)
- * - combine uncomplete stacks before sort
+ * - fix empty slots (air slots) being weird
  * - configurable/custom sorting order selection
  */
 
-const inv = Player.openInventory()
-const sections = {
-    main: [9,35],
-    hotbar: [36, 44],
-}
+const chat = require("../chat/chat")
+const { invSection } = require("../lib/inventory")
+const { itemId, isAir } = require("../lib/item")
+const {Logger} = require("../lib/Logger")
 
-function bubbleSort(startSlot, endSlot) {
-    for (let i = startSlot; i < endSlot + 1; i++) {
-        for (let j = startSlot; j < endSlot; j++) {
-            if ( inv.getSlot(j).getName().getString() < inv.getSlot(j+1).getName().getString() )
-                inv.swap(j, j+1)
-        }
-    }
-}
 
-function partition(low, high) {
-    let pivot = inv.getSlot(high)
-
-    let i = low - 1
-    for (let j = low; j <= high - 1; j++) {
-
-        if (inv.getSlot(j).getName().getString() < pivot.getName().getString()) {
-            i++;
-            inv.swap(i, j)
-        }
-    }
-    inv.swap(i+1, high)
-    Client.waitTick(1)
-    return i+1
-}
+const logger = new Logger("Inventory sorter", "sorter.log", Logger.llog.debug)
 
 /**
- * BROKEN BROKEN BROKEN
- * @param {*} low 
- * @param {*} high 
+ * 
+ * @param {number} itemStackA 
+ * @param {number} itemStackB 
+ * @return  -1 if slot A is before slot B
+ *          0 if slots are equal
+ *          1 if slot A is after slot B
  */
-function quickSort(low, high) {
-    if (low < high) {
-        let pi = partition(low, high)
-
-        quickSort(low, pi - 1)
-        quickSort(pi + 1, high)
+function cmpFunc(slotNumA, slotNumB) {
+    const inv = Player.openInventory()
+    const itemA = inv.getSlot(slotNumA)
+    const itemB = inv.getSlot(slotNumB)
+    if (isAir(itemA) && isAir(itemB)) {
+        //return 0
     }
+    if (isAir(itemA)) {
+        return 1
+    }
+    if (isAir(itemB)) {
+        return -1
+    }
+    return itemId(itemA).localeCompare(itemId(itemB))
 }
 
-function sort(startSlot, endSlot) {
-    bubbleSort(startSlot, endSlot)
+function sortInv(section) {
+    Chat.log(`Sorting ${section}`)
+    const SLOTS = [...invSection(section)]
+    let start = [...SLOTS]
+    const goal = [...SLOTS]
+    goal.sort(cmpFunc)
+
+    Chat.log(`Start slots: ${start.join(" ")}`)
+    Chat.log(`Goal slots:  ${goal.join(" ")}`)
+
+    const base = Math.min(...SLOTS)
+    const destinationForSlot = (slotNum) => base + goal.indexOf(slotNum)
+
+    let actions = new Map() // something wrong with empty slots
+    for (let i = 0; i < start.length; i++) {
+        if (start[i] != goal[i])
+            actions.set(start[i], destinationForSlot(start[i]))
+    }
+    Chat.log(`Action list: (${[...actions].join("),  (")})`)
+
+    const inv = Player.openInventory()
+    while (actions.size > 0) {
+        let pos = [...actions][0][0]
+        let dest = actions.get(pos)
+
+        inv.click(pos) // pick up wrong item
+        inv.click(dest) //place down 
+        actions.delete(pos)
+        Client.waitTick()
+
+        while(!isAir(inv.getHeld())) { // still holding an item
+            pos = dest
+            dest = actions.get(pos)
+            inv.click(dest)
+            actions.delete(pos)
+            Client.waitTick()
+        }
+    }
+
+    Chat.log("Done sorting.")
+    World.playSound("entity.villager.celebrate", 1, 1)
+
 }
 
-sort(...sections.hotbar)
-sort(...sections.main)
+sortInv("hotbar")
