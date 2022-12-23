a9fda8f1bda8dff21cf2a637b36323bbfbc1dbed
Aemaeth <screwthisusernameprocess@gmail.com>
2022-12-23 12:45:33 -0500

    item pickup

--- chat/item-pickup.ts
+++ chat/item-pickup.ts
@@ -1,7 +1,8 @@
-// log a chat message for every item you pick up
+/**
+ * Upon picking up an item, logs information about the item.
+ */
 import { Logger, LogOptions } from "../lib/Logger";
 const logger: Logger = new Logger("pickup", "pickup.log", LogOptions.positioning | LogOptions.direction)
 
-// sent as script to be triggered by "recvmessage" event
 const i = (event as Events.ItemPickup).item;
-logger.log(`You picked up ${i.getCount()} ${i.getName().getString()}`);
+logger.Info(`You picked up ${i.getCount()} ${i.getName().getString()}`);
