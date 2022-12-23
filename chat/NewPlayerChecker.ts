bb4e1efdb8ca5048f0fd993acf3e40a97f58b65b
Aemaeth <screwthisusernameprocess@gmail.com>
2022-12-23 17:19:01 -0500

    namechecker rewrite

---
+++ chat/NewPlayerChecker.ts
@@ -0,0 +1,17 @@
+import { Logger } from "../lib/Logger"
+import { findText } from "./MessageLooker"
+
+const logger: Logger = new Logger("NEw player namechecker", "namechecker.log")
+
+function check() {
+    const content: string[] = (event as Events.RecvMessage).text.getString().split(" ")
+    const poss_name = content.shift()
+    const TARGET_PART: string = "is brand new!"
+    if (!findText(TARGET_PART, content.join(' ')))
+        return
+
+    logger.Debug((event as Events.RecvMessage).text.getString())
+    logger.Prod(`Check name: https://namemc.com/search?q=${poss_name}`)
+}
+
+check()
