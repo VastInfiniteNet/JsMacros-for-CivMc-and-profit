ee6ebf7e89a664a7de254ea973295998739c971e
VastInfiniteNet <84341407+VastInfiniteNet@users.noreply.github.com>
2024-05-03 18:19:53 -0400

    Delete lib directory
    Old, outdated, and illegal in some spots.

--- lib/Chat.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Functions related to sending, receiving, reading, saving chat messages. 
- */
-
-export function setChatGroup(group:string) {
-    const command = `/g ${group}`
-    Chat.say(command)
-}
--- lib/Types.ts
+++
@@ -1,3 +0,0 @@
-export type Delegate<I,J,K> = (itemA: I, itemBList: J[]) => K
-
-export type EventListener = Java.xyz.wagyourtail.jsmacros.core.event.IEventListener
